3e459d4c324668e6c97bedf2ceba4b20ad5d55f0
ianSama0217 <ian20000217@gmail.com>
2023-11-23 12:11:27 +0800

    use search API

--- src/views/SearchAllView.vue
+++ src/views/SearchAllView.vue
@@ -1,5 +1,38 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import api from "../api/index.js";
+/* 匯入組件 */
+
+const { getQuizFrontend, getQuizBackend, getQuizInfo } = api;
+
+const inputTitle = ref("");
+
+const getQuizValue = ref([]);
+
+const search = (inputTitle) => {
+  getQuizFrontend(inputTitle)
+    .then((data) => {
+      getQuizValue.value.push(data);
+    })
+    .catch((e) => {
+      console.log(e);
+      throw e;
+    });
+};
+</script>
 
 <template>
-  <h1>後台</h1>
+  <!-- 搜尋區域 -->
+  <div class="searchBar">
+    <input type="search" v-model="inputTitle" />
+
+    <button type="button" @click="search(inputTitle)">搜尋</button>
+  </div>
+  <!-- 搜尋區域 -->
+
+  <h1>測試搜尋功能:</h1>
+  <p>///{{ getQuizValue[0] }}///</p>
+  <p>///{{ getQuizValue[1] }}///</p>
+  <p>///{{ getQuizValue[2] }}///</p>
+  <p>///{{ getQuizValue[3] }}///</p>
 </template>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,5 +1,52 @@
-<script setup></script>
+<script setup>
+import { ref, onBeforeMount } from "vue";
+import api from "../api/index.js";
+/* 匯入組件 */
+import resList from "../components/search/resList.vue";
+
+const { getQuizFrontend, getQuizInfo } = api;
+
+const inputTitle = ref("");
+
+const getQuizValues = ref([]);
+
+const search = (inputTitle) => {
+  getQuizFrontend(inputTitle)
+    .then((data) => {
+      getQuizValues.value = data;
+    })
+    .catch((e) => {
+      console.log(e);
+      throw e;
+    });
+};
+
+/*載入頁面前先執行一次search，顯示全部資料*/
+onBeforeMount(() => {
+  search("");
+});
+</script>
 
 <template>
-  <h1>前台</h1>
+  <!-- 搜尋區域 -->
+  <div class="searchBar">
+    <input type="search" v-model="inputTitle" />
+
+    <label for="">開始日期</label>
+    <input type="date" />
+    <label for="">結束日期</label>
+    <input type="date" />
+    <button type="button" @click="search(inputTitle)">搜尋</button>
+  </div>
+  <!-- 搜尋區域 -->
+
+  <!-- 顯示搜尋問卷列表 -->
+  <resList :getValue="getQuizValues" />
+  <!-- 顯示搜尋問卷列表 -->
+
+  <!-- 測試區域 -->
+  <!-- <p v-for="(data, index) in getQuizValues" :key="index">
+    ///{{ data.title }}///{{ data.start_time }}///{{ data.end_time }}
+  </p>
+  <h5>getQuizValues:{{ getQuizValues }}</h5> -->
 </template>
